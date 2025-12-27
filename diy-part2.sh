9da55050d5eb70dae0a897b8b5cd5998de380f84
xchename <147893443+xchename@users.noreply.github.com>
2025-12-27 20:46:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-# sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/luci/bin/config_generate
 
 # Modify default theme
